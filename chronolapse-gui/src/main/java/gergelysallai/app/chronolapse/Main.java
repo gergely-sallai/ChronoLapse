b49dff52d0373440211136fb3e6add9480e012eb
Solymosi Tamas <solymosi.tamas@ustream.tv>
2014-09-30 12:33:58 +0200

    gui project

---
+++ chronolapse-gui/src/main/java/gergelysallai/app/chronolapse/Main.java
@@ -0,0 +1,12 @@
+package gergelysallai.app.chronolapse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class.getName());
+
+    public static void main(String[] args) {
+        logger.info("Main");
+    }
+}
